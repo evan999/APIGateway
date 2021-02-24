068f40f3eff00426ca2b1c4edf9762bd80cb2930
evan999 <emeshberge@gmail.com>
2021-02-23 19:31:31 -0500

    Remove unnecessary comments.

--- src/main/java/com/apiGateway/ApiGatewayApplication.java
+++ src/main/java/com/apiGateway/ApiGatewayApplication.java
@@ -21,42 +21,6 @@ public class ApiGatewayApplication {
 	//private static final String POSTS_API_URL = "";
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-
-//		HttpClient client = HttpClient.newHttpClient();
-////				.version(HttpClient.Version.HTTP_1_1)
-////				.followRedirects(HttpClient.Redirect.NORMAL)
-////				.build();
-//
-//		//HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//		//System.out.println(response.statusCode());
-//		//System.out.println(response.body());
-//
-//
-//		HttpRequest request = HttpRequest.newBuilder()
-//				.uri(URI.create("https://newsapi.org/v2/sources?apiKey=38b94640783949fd971bc7b04f93226a"))
-//				.header("Content-Type", "application/json")
-//				.GET()
-//				.build();
-//		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-		/*
-		client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-				.thenApply(HttpResponse::body)
-				.thenAccept(System.out::println);
-
-		 */
-		/*
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-				.GET()
-				.header("Content-Type", "application/json")
-				.uri(URI.create(POSTS_API_URL))
-				.build();
-		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-		System.out.println(response.body());
-		 */
-
 		SpringApplication.run(ApiGatewayApplication.class, args);
 //		GetRoute route = new GetRoute();
 //		route.getRoute();
@@ -67,13 +31,6 @@ public class ApiGatewayApplication {
 	@GetMapping("/")
 	public String getRoute() throws IOException, InterruptedException{
 		HttpClient client = HttpClient.newHttpClient();
-//				.version(HttpClient.Version.HTTP_1_1)
-//				.followRedirects(HttpClient.Redirect.NORMAL)
-//				.build();
-
-		//HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		//System.out.println(response.statusCode());
-		//System.out.println(response.body());
 
 
 		HttpRequest request = HttpRequest.newBuilder()
@@ -85,48 +42,4 @@ public class ApiGatewayApplication {
 
 		return response.body();
 	}
-//
-////	@Value("${postUrl}")
-////	private String postUrl;
-//	@PostMapping(path = "/posts", consumes = "application/json", produces = "application/json")
-//	public String postRoute(@RequestBody String request) throws IOException, InterruptedException {
-//		HttpClient client = HttpClient.newHttpClient();
-//		HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(request);
-////				.version(HttpClient.Version.HTTP_1_1)
-////				.followRedirects(HttpClient.Redirect.NORMAL)
-////				.build();
-//
-//		//HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//		//System.out.println(response.statusCode());
-//		//System.out.println(response.body());
-//
-//
-//		HttpRequest postRequest = HttpRequest.newBuilder()
-//				.uri(URI.create("http://jsonplaceholder.typicode.com/posts"))
-//				.header("Content-Type", "application/json")
-//				.POST(body)
-//				.build();
-//		HttpResponse<String> response = client.send(postRequest, HttpResponse.BodyHandlers.ofString());
-//
-//		return response.body();
-//	}
-
-//
-//	@GetMapping("/post")
-//	public Object postRoute(@RequestBody String request, @PathVariable String location) throws Exception, IOException{
-//
-//		HttpClient httpClient = HttpClient.newHttpClient();
-//		HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(request);
-//
-//		HttpRequest serverRequest = HttpRequest.newBuilder()
-//				.POST(body)
-//				.header("Content-Type", "application/json")
-//				.uri(URI.create(POSTS_API_URL))
-//				.build();
-//		HttpResponse<String> response = httpClient.send(serverRequest, HttpResponse.BodyHandlers.ofString());
-//
-//		return response.body();
-
-//	}
-
 }

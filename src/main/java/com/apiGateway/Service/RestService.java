5fae5ca9a0996d40f9b32d65db6f70f146a141c5
evan999 <emeshberge@gmail.com>
2021-02-25 18:44:44 -0500

    Add in method to Create Post in RestService.

--- src/main/java/com/apiGateway/Service/RestService.java
+++ src/main/java/com/apiGateway/Service/RestService.java
@@ -2,8 +2,20 @@ package com.apiGateway.Service;
 
 import com.apiGateway.Model.Post;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+//@RestController
 public class RestService {
 
     private final RestTemplate restTemplate;
@@ -12,20 +24,55 @@ public class RestService {
         this.restTemplate = restTemplateBuilder.build();
     }
 
+//    @RequestMapping("/api/posts")
     public String getPostsPlainJSON() {
         String url = "https://jsonplaceholder.typicode.com/posts";
         return this.restTemplate.getForObject(url, String.class);
     }
+
 
     public Post[] getPostsAsObject() {
         String url = "https://jsonplaceholder.typicode.com/posts";
         return this.restTemplate.getForObject(url, Post[].class);
     }
 
+
     public Post getPostWithUrlParameters() {
         String url = "https://jsonplaceholder.typicode.com/posts/{id}";
         return this.restTemplate.getForObject(url, Post.class, 1);
     }
 
-    
+    public Post getPostWithResponseHandling() {
+        String url = "https://jsonplaceholder.typicode.com/posts/{id}";
+        ResponseEntity<Post> response = this.restTemplate.getForEntity(url, Post.class, 1);
+
+        if(response.getStatusCode() == HttpStatus.OK) {
+            return response.getBody();
+        }
+        else {
+            return null;
+        }
+    }
+
+    public Post createPost(int userID, String title, String body) {
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        HttpHeaders headers = new HttpHeaders();
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("userId", userID);
+        map.put("title", title);
+        map.put("body", body);
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(map, headers);
+
+        ResponseEntity<Post> response = this.restTemplate.postForEntity(url, entity, Post.class);
+
+        if (response.getStatusCode() == HttpStatus.CREATED) {
+            return response.getBody();
+        } else {
+            return null;
+        }
+    }
 }
